f5464709f37ef14a0bdab2d0a73dc0df306a39ac
tejas244 <kholetejas162@gmail.com>
2025-09-14 23:40:32 +0530

    first part done - using Spring - webflux

---
+++ email-writer-sb/src/main/java/com/email/writer/EmailGeneratorController.java
@@ -0,0 +1,25 @@
+package com.email.writer;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/email")
+
+public class EmailGeneratorController {
+
+    @Autowired
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
